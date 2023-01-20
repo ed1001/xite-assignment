203dddb1433ca3edce7a9d8efdb8d66f907e067f
Edward Phillips <edphil@hotmail.co.uk>
2023-01-20 23:49:44 +0100

    sort tracks by date

--- src/api/xite.ts
+++ src/api/xite.ts
@@ -7,9 +7,19 @@ export const getTracks = async (): Promise<Track[]> => {
   try {
     const res = await fetch(TRACKS_ENDPOINT_URL);
 
-    return await res.json();
+    const tracksUnsorted: Track[] = await res.json();
+    const tracks = tracksUnsorted.sort((a, b) => {
+      const dateA = new Date(a.createdAt);
+      const dateB = new Date(b.createdAt);
+      if (dateA === dateB) {
+        return 0;
+      }
+
+      return dateA > dateB ? -1 : 1;
+    });
+
+    return tracks;
   } catch (err) {
-    // TODO handle error
     return [];
   }
 };
