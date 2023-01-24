6d8a8e91b446f8f949f4befdd807b5e79d84b98e
Edward Phillips <edphil@hotmail.co.uk>
2023-01-24 11:41:49 +0100

    add scroll to element hook

---
+++ src/hooks/useScrollToAddedOrActiveElement.tsx
@@ -0,0 +1,44 @@
+import { useCallback } from "react";
+import partition from "lodash.partition";
+
+const useScrollToAddedOrActiveElement = () => {
+  return useCallback((node: HTMLDivElement) => {
+    if (node) {
+      const observer = new MutationObserver((mutations) => {
+        const [attributeMutations, nodeAddedMutations] = partition(
+          mutations,
+          (mutation) => mutation.type === "attributes"
+        );
+
+        let elementToScrollTo;
+
+        if (!!nodeAddedMutations.length) {
+          const [{ addedNodes }] = nodeAddedMutations;
+          elementToScrollTo = addedNodes[addedNodes.length - 1] as HTMLElement;
+        }
+
+        if (!elementToScrollTo) {
+          const activeElement = attributeMutations.find((mutation) =>
+            [...(mutation.target as HTMLElement).classList]
+              .join("")
+              .includes("active")
+          );
+          elementToScrollTo = activeElement?.target as HTMLElement;
+        }
+
+        elementToScrollTo?.scrollIntoView({
+          behavior: "smooth",
+          inline: "nearest",
+        });
+      });
+
+      observer.observe(node, {
+        childList: true,
+        attributes: true,
+        subtree: true,
+      });
+    }
+  }, []);
+};
+
+export default useScrollToAddedOrActiveElement;
